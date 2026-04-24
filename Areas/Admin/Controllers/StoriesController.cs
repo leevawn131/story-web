796ea6909492884c00651156bdf8e6ed7ff86b35
leevawn131 <ledinhvan205@gmail.com>
2026-04-24 15:23:58 +0700

    admin xóa truyện thành công

--- Areas/Admin/Controllers/StoriesController.cs
+++ Areas/Admin/Controllers/StoriesController.cs
@@ -68,4 +68,37 @@ public class StoriesController : Controller
         TempData["SuccessMessage"] = $"Story '{story.StoryName}' has been rejected.";
         return RedirectToAction(nameof(Index));
     }
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var story = _context.Stories.Find(id);
+        if (story is null)
+        {
+            return NotFound();
+        }
+
+        return View(story);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id_Story)
+    {
+        var story = _context.Stories.Find(id_Story);
+        var c = _context.Chapters.Where(x => x.id_Story == id_Story).ToList();
+        var read = _context.ReadingHistories.Where(x => x.id_Story == id_Story).ToList();
+        var love = _context.Favourites.Where(x => x.id_Story == id_Story).ToList();
+        if (story is null)
+        {
+            return NotFound();
+        }
+        
+        _context.ReadingHistories.RemoveRange(read);
+        _context.Favourites.RemoveRange(love);
+        _context.Chapters.RemoveRange(c);
+        _context.Stories.Remove(story);
+        _context.SaveChanges();
+        TempData["SuccessMessage"] = "Xoa truyện thanh cong.";
+        return RedirectToAction(nameof(Index));
+    }
 }
